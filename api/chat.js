e06b5a299ab785f16d6baf01dfcc585d8c6ee453
pontepath59 <PontePath@gmail.com>
2026-04-21 23:11:25 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       return res.status(405).json({ error: "Method not allowed" });
 
     const { message } = req.body;
-    c
+    
 
     if (!message) {
 
