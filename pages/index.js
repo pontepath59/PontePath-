41d4dfd7c6bdd0d86f9b31be2db162ef3bdfb7e4
pontepath59 <PontePath@gmail.com>
2026-04-29 07:39:20 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,1 @@
-export default function Home() {
 
-  return (
-
-    <div style={{padding: 40}}>
-
-      <h1>PontePath is Live 🙏</h1>
-
-      <p>Your app is working.</p>
-
-    </div>
-
-  );
-
-}
